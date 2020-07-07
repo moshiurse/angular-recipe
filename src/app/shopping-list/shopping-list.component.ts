6ef80f9fa9ba79f9a8b413d403b02f8259bac64f
Moshiur Rahman <rmoshiur705@gmail.com>
2020-07-07 13:14:22 +0600

    Ingredints Model & Shopping Ui added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Salad', 10),
+    new Ingredient('Apple', 5),
+    new Ingredient('carrot', 8)
+  ];
 
   constructor() { }
 
